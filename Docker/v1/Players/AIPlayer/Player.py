60002a6e185aa928de9d79e0c00cd43fd6940321
Knight.Zhou <YoungWelle@gmail.com>
2017-06-28 00:05:14 +0800

    FIXED:invalid game message issue;

--- Docker/v1/Players/AIPlayer/Player.py
+++ Docker/v1/Players/AIPlayer/Player.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
             print("v_player2_id:", v_player2_id)
             logging.info("VS game id:%s,player1_id:%s,player2_id:%s", v_game_id,v_player1_id,v_player2_id)
             # subscribe the game topic
-            client.subscribe(v_game_id)
+            client.subscribe(v_game_id,2)
             logging.info("MQTT subscribe to:%s", v_game_id)
             #  testing
             # client.publish(v_game_id, "594a4c8b6516899e6a30e17f#play#B[cm]")
@@ -152,12 +152,12 @@ if __name__ == '__main__':
                 # next round as opponent only
                 if cur_player_id != v_player_id:
                     logging.info("PLAY current player id:%s,v_player_id:%s",cur_player_id,v_player_id)
-                    # message assemble
-                    message_json = {'game_id': v_game_id,
+                    # message assemble for sending
+                    message_json_send = {'game_id': v_game_id,
                                'user_id': cur_player_id,
                                'msg': pre_play_msg}
                     # append message
-                    message_json_s.append(message_json)
+                    message_json_s.append(message_json_send)
                     print("assembled message_json_s:", message_json_s)
                     # msg response
                     logging.info("send to plainAI message_json_s:%s",message_json_s)
@@ -165,11 +165,17 @@ if __name__ == '__main__':
                     result['user_id'] = v_player_id
                     # result['method'] = 'play'
                     logging.info('PLAY plainAI response:%s', result)
-                    # append msa
+                    # message assemble for response
+                    message_json_resp = {'game_id': v_game_id,
+                                         'user_id': v_player_id,
+                                         'msg': result['msg']}
+                    # append message
+                    message_json_s.append(message_json_resp)
                     # publish message
                     client.publish(v_game_id, v_player_id + v_tag_play + result['msg'])
                     # client.publish(v_game_id,json.dumps(result))
-
+                else:
+                    logging.info("##PLAY current player:%s,pre_play_msg:%s", cur_player_id, pre_play_msg)
 
     # The callback for when this client publishes to the server.
     def on_publish(client, userdata, mid):
@@ -190,7 +196,7 @@ if __name__ == '__main__':
 
     client.connect(v_broker_url, v_broker_port, 60)
     #default channel name.
-    client.subscribe(v_client_id,0)
+    client.subscribe(v_client_id,2)
 
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
